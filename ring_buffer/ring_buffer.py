46004bc52b27931f6453d99ab362714a9dd326d9
unknown <Dustin.Guillen14@yahoo.com>
2020-07-31 19:59:35 -0600

    ring_buffer and reverse

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,22 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity 
+        ### ===== current is the index as we progress through the list =====
+        self.current = 0
+        self.data = []
 
     def append(self, item):
-        pass
+        if len(self.data) == self.capacity:
+            print(f"data: {self.data}, current:{self.current}")
+            self.data[self.current] = item
+
+            if [self.current] == [self.capacity-1]:
+                self.current = 0
+            else:
+                self.current += 1
+                
+        else:
+            self.data.append(item)
 
     def get(self):
-        pass
+        return self.data
